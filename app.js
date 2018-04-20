b1bb53365112abd913fcd5bbb796bd3a5c94aed6
David Pineda <david.pineda@deparadise.com>
2018-04-20 01:21:04 -0700

    add a public assets directory

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// ROOT
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
