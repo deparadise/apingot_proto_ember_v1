b1f085d121a816a01e2a14d0cd57d0a8b6025100
David Pineda <dpineda@hagroup.com>
2018-06-14 00:10:04 -0700

    using import module, include body-parser for POST data, notes & clean up, setup ENV dynamic port or static preset

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const express = require('express');
+import express from 'express';
+const app = express();
 
+// Use BP for access to POST data
+import bodyParser from 'body-parser';
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Include api module
 import api from './api';
 
-const app = express();
-// const router = express.Router();
+// ROOT serves FE_shovel client...
 const path = require('path');
-
-// FE ROOT serves client...
 app.use(express.static(__dirname + '/client'));
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
+// Routes...
 app.use('/api', api);
 
 // START APP...
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const port = process.env.PORT || 3000; // ENV dynamic port or static preset
+app.listen(port, () => console.log('Example app listening on port:', port));
