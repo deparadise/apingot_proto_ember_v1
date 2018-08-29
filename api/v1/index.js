28f8553aa24f304af86b8434af385c4e5abdac4f
David Pineda <david.pineda@deparadise.com>
2018-08-28 23:41:54 -0700

    hold off on auth

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,7 +1,7 @@
 console.log('>> Using /v1');
 import express from 'express';
 
-import auth from './auth';
+// import auth from './auth';
 // import testing from '../testing';
 
 const v1 = express.Router();
@@ -16,7 +16,7 @@ v1.get('/', (req, res) => {
 	res.json(v1.versionMeta);
 });
 
-v1.use('/auth', auth);
+// v1.use('/auth', auth);
 // v1.use('/test', testing);
 
 module.exports = v1;
